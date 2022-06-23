fac752a315fcb9a781447dd4947d2c30b353038c
Matthew Mikhaiel <MattDFD@live.ca>
2022-06-23 14:28:59 +0200

    heroku running

--- server/config/db.js
+++ server/config/db.js
@@ -3,3 +3,6 @@ module.exports = {
   "URI": "mongodb+srv://mattdfd:W5QXMcdKnFL7U9mj@assignment2.bv2qf.mongodb.net/?retryWrites=true&w=majority"
 };
 //mongodb+srv://mattdfd:W5QXMcdKnFL7U9m@assignment2.bv2qf.mongodb.net/?retryWrites=true&w=majority
+
+
+//heroku link: https://comp229-2020-midterm-301229715.herokuapp.com/books
